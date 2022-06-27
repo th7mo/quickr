0e734ecce34afd9bad2d71b5561899e561390d4a
th7mo <thimo@th7mo.nl>
2022-06-27 14:36:48 +0200

    ✅ add tests to check init of QRCode size

--- lib/th7mo_quickr_qr/src/lib.rs
+++ lib/th7mo_quickr_qr/src/lib.rs
@@ -44,4 +44,39 @@ mod tests {
 
         assert_eq!(qr_v1.size, VERSION_1_SIZE);
     }
+
+    #[test]
+    fn should_have_size_25_for_version_2() {
+        const VERSION_2_SIZE: u8 = 25;
+        let qr_v2 = QRCode::new(2);
+
+        assert_eq!(qr_v2.size, VERSION_2_SIZE);
+    }
+
+
+    #[test]
+    fn should_have_size_57_for_version_10() {
+        const VERSION_10_SIZE: u8 = 57;
+        let qr_v10 = QRCode::new(10);
+
+        assert_eq!(qr_v10.size, VERSION_10_SIZE);
+    }
+
+
+    #[test]
+    fn should_have_size_117_for_version_25() {
+        const VERSION_25_SIZE: u8 = 117;
+        let qr_v25 = QRCode::new(25);
+
+        assert_eq!(qr_v25.size, VERSION_25_SIZE);
+    }
+
+
+    #[test]
+    fn should_have_size_177_for_version_40() {
+        const VERSION_40_SIZE: u8 = 177;
+        let qr_v40 = QRCode::new(40);
+
+        assert_eq!(qr_v40.size, VERSION_40_SIZE);
+    }
 }
