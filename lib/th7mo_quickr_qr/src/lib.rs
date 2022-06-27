63e414c56a7598edce444479cb3e61c5cd95a636
th7mo <dev@th7mo.com>
2022-06-27 13:41:38 +0200

    🚧 make basic implementation for QRCode with Bits

--- lib/th7mo_quickr_qr/src/lib.rs
+++ lib/th7mo_quickr_qr/src/lib.rs
@@ -1,8 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+#[derive(Debug)]
+pub struct Bit {
+    pub on: bool,
+    pub reserved: bool,
+}
+
+#[derive(Debug)]
+pub struct QRCode {
+    version: u8,
+    bits: Vec<Vec<Bit>>,
+}
+
+impl QRCode {
+    pub fn new(version: u8) -> Self {
+        QRCode {
+            version,
+            bits: vec![
+                vec![
+                    Bit { on: false, reserved: false },
+                    Bit { on: false, reserved: false },
+                    Bit { on: true, reserved: false },
+                ]
+            ],
+        }
     }
 }
